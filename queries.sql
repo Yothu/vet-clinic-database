2ad8329a903bbb95b3fd3a14c77c9edeb1d9a24e
David Vergaray <david.vergaray.a@gmail.com>
2022-03-22 13:51:03 -0300

    Add verification to transaction

--- queries.sql
+++ queries.sql
@@ -32,7 +32,9 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 /* Inside a transaction... */
 BEGIN;
